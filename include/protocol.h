b5e13bc9892af2e2285a000329c5fe7b739c9865
adiwang <adiwang@outlook.com>
2016-01-27 14:51:35 +0800

    调整了sieve的协议处理方式
    1. protocol的Process函数增加了userdata参数，用来传递相应的上下文
    2. Process不再返回string，而是直接在Process内根据需要进行协议发送
    3. 修改了相应的测试文件

--- include/protocol.h
+++ include/protocol.h
@@ -1,25 +1,18 @@
 ﻿/***************************************
-* @file     tcpserverprotocolprocess.h
-* @brief    TCP Server protocol pure base class,need to subclass.
+* @file     protocol.h
+* @brief    protocol pure base class,need to subclass.
 * @details  
-* @author   phata, wqvbjhc@gmail.com
-* @date     2014-7-31
+* @author   adiwang, adiwang@outlook.com
 ****************************************/
 #ifndef _PROTOCOL_H
 #define _PROTOCOL_H
-#include <string>
 
 class Protocol
 {
 public:
     Protocol(){}
     virtual ~Protocol(){}
-
-    //parse the recv packet, and make the response packet return. see test_tcpserver.cpp for example
-	//packet     : the recv packet
-	//buf        : the packet data
-	//std::string: the response packet. no response can't return empty string.
-    virtual const std::string& Process(const char* buf, int length) = 0;
+    virtual void Process(const char* buf, int length, void* userdata) = 0;
 };
 
 #endif//_PROTOCOL_H
