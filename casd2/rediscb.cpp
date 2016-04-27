b858103bf8bfd9076e6c942fc2bd81a7670d0602
adiwang <adiwang@outlook.com>
2016-04-27 17:15:37 +0800

    修改了redis连接逻辑
    当收到连接回调时向数据库发送查询样本库的命令，当收到响应时加载样本库

--- casd2/rediscb.cpp
+++ casd2/rediscb.cpp
@@ -1,8 +1,10 @@
 #include "rediscb.h"
 #include "stdio.h"
 #include "tcpserver.h"
+#include "dataman.h"
 #include <cstdio>
 
+redisAsyncContext* redis_context;
 
 void OnConnect(const redisAsyncContext *c, int status) 
 {
@@ -11,6 +13,8 @@ void OnConnect(const redisAsyncContext *c, int status)
         return;
     }
     printf("Connected...\n");
+	// 加载数据, 获取样本库所有数据
+	redisAsyncCommand(c, GetSamplesCB, NULL, "HVALS samples");
 }
 
 void OnDisconnect(const redisAsyncContext *c, int status)
@@ -22,4 +26,16 @@ void OnDisconnect(const redisAsyncContext *c, int status)
     printf("Disconnected...\n");
 }
 
-redisAsyncContext* redis_context;
+void GetSamplesCB(redisAsyncContext *c, void *r, void *privdata)
+{
+	redisReply *reply = r;
+	if(reply == NULL) return;
+	size_t sample_count = reply->elements;
+	for(size_t i = 0; i < sample_count; ++i)
+	{
+		DataMan::GetInstance().AddSample(reply->element[i].str);
+	}
+	//TODO: 这里调用处理类加载数据
+}
+
+
