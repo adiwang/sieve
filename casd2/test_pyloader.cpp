fc92316e85fce7cfe6d3e5991812b760e956b277
adiwang <adiwang@outlook.com>
2016-05-15 11:07:43 +0800

    update test_pyloader.cpp

--- casd2/test_pyloader.cpp
+++ casd2/test_pyloader.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
     PyLoader::GetInstance().Init();
     PyLoader::GetInstance().Load("pytest");
+    PyLoader::GetInstance().Load("leaf_grade");
     PyObject* pArgs = PyTuple_New(2);
     PyTuple_SetItem(pArgs, 0, Py_BuildValue("i",3));
     PyTuple_SetItem(pArgs, 1, Py_BuildValue("i",4));
@@ -18,5 +19,11 @@ int main(int argc, char *argv[])
     cout << "call test.py add fucntion: 3 + 4 = " << retval << endl;
     PyObject* pIns = PyLoader::GetInstance().CreateClassInstance("pytest", "guestlist");
     PyLoader::GetInstance().CallInstanceMethod(pIns, "p");
+
+    PyObject* pLGIns = PyLoader::GetInstance().CreateClassInstance("leaf_grade", "LeafGrade");
+    if(pLGIns)
+        cout << "create ins success" << endl;
+    else
+        cout << "create ins err" << endl;
     return 0;
 }
