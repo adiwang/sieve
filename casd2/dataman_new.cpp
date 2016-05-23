ed201e8ba6013312e84209650c0449ead1a589ba
adiwang <adiwang@outlook.com>
2016-05-23 17:59:57 +0800

    增加了dataman_new

---
+++ casd2/dataman_new.cpp
@@ -0,0 +1,261 @@
+#include "dataman.h"
+#include <cstdlib>
+#include <exception>
+#include <sstream>
+#include <iostream>
+#include "log.h"
+
+namespace CASD
+{
+
+DataMan::~DataMan()
+{
+    // 清除样本库
+	for(GroupRankMapIter git = _samples.begin(); git != _samples.end(); ++git)
+    {
+        for(RankFeatureListMapIter rit = git->second.begin(); rit != git->second.end(); ++rit)
+        {
+			for(FeatureListIter fit = rit->second.begin(); fit != rit->second.end(); ++fit)
+			{
+				if(*fit)
+				{
+					delete (*fit);
+					*fit = NULL;
+				}
+			}
+        }
+    }
+    _samples.clear();
+	// 释放python中类的实例
+	if(_leafgrade_instance)
+	{
+		Py_DECREF(_leafgrade_instance);
+		_leafgrade_instance = NULL;
+	}
+}
+
+std::string DataMan::GroupRankMap2Json(const GroupRankMap& group2rank)
+{
+    std::string jsonstr;
+    try
+    {
+        Json::Value value;
+        std::stringstream ss;
+        for(GroupRankMapIter git = group2rank.begin(); git != group2rank.end(); ++git)
+        {
+            Json::Value r2f_value;
+            for(RankFeatureListMapIter rit = git->second.begin(); rit != git->second.end(); ++rit)
+            {
+                Json::Value fl_value;
+                for(FeatureListIter fit = rit->second.begin(); fit != rit->second.end(); ++fit)
+                {
+                    Json::Value f_value;
+                    LeafFeature2Json(*(*fit), f_value);
+                    fl_value.append(f_value);
+                }
+                ss.str("");
+                ss << rit->first;
+                r2f_value[ss.str()] = fl_value;
+            }
+            ss.str("");
+            ss << git->first;
+            value[ss.str()] = r2f_value;
+        }
+        jsonstr = value.toStyledString();
+    }
+    catch(std::exception &ex)
+    {
+        std::cerr << "DataMan::GroupRankMap2Json exception: " << ex.what() << std::endl;
+        return NULL;
+    }
+    return jsonstr;
+}
+    
+int DataMan::Json2GroupRankMap(const std::string& jsonstr, GroupRankMap& group2rank)
+{
+    group2rank.clear();
+    try
+    {
+        Json::Reader reader;
+        Json::Value value;
+        if(!reader.parse(jsonstr, value))       
+        {
+            std::cerr << "DataMan::Json2GroupRankMap parse error!\n" << jsonstr << std::endl;
+			LOG_TRACE("DataMan::Json2GroupRankMap parse error!json:\n%s", jsonstr.c_str());
+            return 1;
+        }
+        Json::Value::Members grp_names = value.getMemberNames();
+        for(Json::Value::Members::iterator git = grp_names.begin(); git != grp_names.end(); ++git)
+        {
+            std::string grp_name = *git;
+            RankFeatureListMap r2fl;
+            Json::Value r2fl_value = value[grp_name];
+            Json::Value::Members r_names = r2fl_value.getMemberNames();
+            for(Json::Value::Members::iterator rit = r_names.begin(); rit != r_names.end(); ++rit)            
+            {
+                std::string r_name = *rit;
+                FeatureList fl;
+                Json::Value fl_value = r2fl_value[r_name];
+                for(unsigned int i = 0; i < fl_value.size(); ++i)
+                {
+                    LeafFeature* pFeature = new LeafFeature();
+                    Json::Value& fval = fl_value[i];
+                    Json2LeafFeature(fval, *pFeature);
+                    fl.push_back(pFeature);
+                }
+                r2fl.insert(std::make_pair(atoi(r_name.c_str()), fl));
+            }
+            group2rank.insert(std::make_pair(atoi(grp_name.c_str()), r2fl));
+        }
+    }
+    catch(std::exception &ex)
+    {
+        std::cerr << "DataMan::Json2GroupRankMap exception: " << ex.what() << std::endl;
+		LOG_TRACE("DataMan::Json2GroupRankMap exception: %s", ex.what());
+        return 1;
+    } 
+    return 0;
+}
+
+void DataMan::LeafFeature2Json(const LeafFeature& feature, Json::Value& value)
+{
+    value["id"] = feature.id;
+    value["group"] = feature.Group;
+    value["rank"] = feature.Rank;
+    value["avg_saturation"] = feature.AvgSaturation;
+    value["avg_hue"] = feature.AvgHue;
+    value["avg_intensity"] = feature.AvgIntensity;
+    value["deviation_saturation"] = feature.DeviationSaturation;
+    value["deviation_hue"] = feature.DeviationHue;
+    value["deviation_intensity"] = feature.DeviationIntensity;
+    value["length"] = feature.Length;
+    value["width"] = feature.Width;
+    value["width_length_ratio"] = feature.WidthLengthRatio;
+    value["apex_angle"] = feature.ApexAngle;
+    value["circularity"] = feature.Circularity;
+    value["area"] = feature.Area;
+    value["thick_mean"] = feature.ThickMean;
+    value["defect_rate"] = feature.DefectRate;
+}
+
+void DataMan::Json2LeafFeature(const Json::Value& value, LeafFeature& feature)
+{
+    feature.id = value["id"].asString();
+    feature.Group = value["group"].asInt();
+    feature.Rank = value["rank"].asInt();
+    feature.AvgSaturation = value["avg_saturation"].asDouble();                 
+    feature.AvgHue = value["avg_hue"].asDouble();                 
+    feature.AvgIntensity = value["avg_intensity"].asDouble();                 
+    feature.DeviationSaturation = value["deviation_saturation"].asDouble();                 
+    feature.DeviationHue = value["deviation_hue"].asDouble();                 
+    feature.DeviationIntensity = value["deviation_intensity"].asDouble();                 
+    feature.Length = value["length"].asDouble();                 
+    feature.Width = value["width"].asDouble();                 
+    feature.WidthLengthRatio = value["width_length_ratio"].asDouble();                 
+    feature.ApexAngle = value["apex_angle"].asDouble();                 
+    feature.Circularity = value["circularity"].asDouble();                 
+    feature.Area = value["area"].asDouble();                 
+    feature.ThickMean = value["thick_mean"].asDouble();                 
+    feature.DefectRate = value["defect_rate"].asDouble(); 
+}
+
+void DataMan::AddSample(const std::string& jsonstr)
+{
+	Json::Reader reader;
+	Json::Value value;
+	if(!reader.parse(jsonstr, value))       
+	{
+		std::cerr << "DataMan::AddSample parse error!\n" << jsonstr << std::endl;
+		LOG_TRACE("DataMan::AddSample parse error! json:\n%s", jsonstr.c_str());
+		return;
+	}
+	int group = value["group"].asInt();
+	int rank = value["rank"].asInt();
+	GroupRankMapIter git = _samples.find(group);
+	if(git == _samples.end())
+	{
+		RankFeatureListMap r2fl;
+		_samples.insert(std::make_pair(group, r2fl));
+		git = _samples.find(group);
+	}
+	RankFeatureListMapIter rit = git->second.find(rank);
+	if(rit == git->second.end())
+	{
+		FeatureList fl;
+		git->second.insert(std::make_pair(rank, fl));
+		rit = git->second.find(rank);
+	}
+	LeafFeature* pFeature = new LeafFeature();
+	Json2LeafFeature(value, *pFeature);
+	rit->second.push_back(pFeature);
+}
+
+void DataMan::AddSample(const LeafFeature& feature)
+{
+	GroupRankMapIter git = _samples.find(feature.Group);
+	if(git == _samples.end())
+	{
+		RankFeatureListMap r2fl;
+		_samples.insert(std::make_pair(feature.Group, r2fl));
+		git = _samples.find(feature.Group);
+	}
+	RankFeatureListMapIter rit = git->second.find(feature.Rank);
+	if(rit == git->second.end())
+	{
+		FeatureList fl;
+		git->second.insert(std::make_pair(feature.Rank, fl));
+		rit = git->second.find(feature.Rank);
+	}
+	LeafFeature* pFeature = new LeafFeature(feature);
+	rit->second.push_back(pFeature);
+}
+
+void DataMan::SetLeafGradeInstance(PyObject* instance)
+{
+	if(_leafgrade_instance)
+	{
+		Py_DECREF(_leafgrade_instance);
+	}
+	_leafgrade_instance = instance;
+}
+
+PyObject* DataMan::GetLeafGradeInstance()
+{
+	return _leafgrade_instance;
+}
+
+std::string DataMan::GetSamplesJson()
+{
+	return GroupRankMap2Json(_samples);
+}
+
+void DataMan::StatisticsSamples(std::vector<LeafGradeCount>& leaf_grade_counts)
+{
+    leaf_grade_counts.clear();
+    for(GroupRankMapIter git = _samples.begin(); git != _samples.end(); ++git)
+    {
+        for(RankFeatureListMapIter rit = git->second.begin(); rit != git->second.end(); ++rit)
+        {
+            LeafGradeCount lgc;
+            lgc.set_group(git->first);
+            lgc.set_rank(rit->first);
+            lgc.set_count(rit->second.size());
+            leaf_grade_counts.push_back(lgc);
+        }
+    }
+}
+
+int DataMan::GetSamplesCount()
+{
+    int res = 0;
+    for(GroupRankMapIter git = _samples.begin(); git != _samples.end(); ++git)
+    {
+        for(RankFeatureListMapIter rit = git->second.begin(); rit != git->second.end(); ++rit)
+        {
+            res += rit->second.size();
+        }       
+    }
+    return res;
+}   // end of namespace CASD
+
+}
---
+++ casd2/dataman_new.h
@@ -0,0 +1,55 @@
+#ifndef _CASD_DATAMAN_H
+#define _CASD_DATAMAN_H
+#include <Python.h>
+#include <vector>
+#include <map>
+#include "common_def.h"
+#include <json/json.h>
+#include "pb/netmessage.pb.h"
+
+namespace CASD
+{
+class DataMan
+{
+public:
+    typedef std::vector<LeafFeature*> FeatureList;                                   // 特征列表
+    typedef std::vector<LeafFeature*>::iterator FeatureListIter;
+    typedef std::map<int, FeatureList> RankFeatureListMap;                          // 等级到特征列表的映射
+    typedef std::map<int, FeatureList>::iterator RankFeatureListMapIter;
+    typedef std::map<int, RankFeatureListMap> GroupRankMap;
+    typedef std::map<int, RankFeatureListMap>::iterator GroupRankMapIter;           // 组别到等级的映射
+
+public:
+    ~DataMan();
+    static DataMan& GetInstance()
+    {
+        static DataMan instance;
+        return instance;
+    }
+
+private:
+    GroupRankMap _samples;					// 样本数据存储结构
+	PyObject*	_leafgrade_instance;		// python脚本中提供的用来学习和分类的类的实例
+
+
+private:
+    DataMan() : _samples(), _leafgrade_instance(NULL) {}
+
+public:
+    std::string GroupRankMap2Json(const GroupRankMap& group2rank);
+    int Json2GroupRankMap(const std::string& jsonstr, GroupRankMap& group2rank);
+    void LeafFeature2Json(const LeafFeature& feature, Json::Value& value);
+    void Json2LeafFeature(const Json::Value& value, LeafFeature& feature);
+	void AddSample(cosnt std::string& jsonstr);
+	void AddSample(const LeafFeature& feature);
+	void SetLeafGradeInstance(PyObject* instance);
+	PyObject* GetLeafGradeInstance();
+	std::string GetSamplesJson();
+    void StatisticsSamples(std::vector<LeafGradeCount>& leaf_grade_counts);
+    int GetSamplesCount();
+};
+
+
+}   // end of namespace CASD
+
+#endif  // end _CASD_DATAMAN_H
